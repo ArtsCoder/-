dc841122f73bcb06e10312b8dcf140843920a826
Your Name <you@example.com>
2023-04-23 14:23:19 +0300

    Третий коммит

---
+++ Задача_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1.на вход принимает два числа 
+// 2. проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number = number2*number2;
+if (number1==number) 
+ {
+     Console.WriteLine("YES");
+ }
+ else
+ {
+   Console.WriteLine("NO");
+ } 
+ 
+
+//Console.WriteLine("Число 1 является Квадртом числа 2); 
---
+++ Задача_3/Program.cs
@@ -0,0 +1,50 @@
+﻿//  выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// Console.WriteLine("Введите порядковый номер дня недели");
+// // number = Console.ReadLine();
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number == 1)
+// {
+//     Console.WriteLine("Понедельник"); 
+//      if (number == 2)
+//      {
+//      Console.WriteLine("Вторник");
+//      }
+//  else 
+//     {
+//     Console.WriteLine("Значение некорректное"); 
+//     }
+// }
+Console.WriteLine("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+switch(number)
+{
+case 1:
+Console.WriteLine("Понедельник");
+break;
+case 2:
+Console.WriteLine("Вторник");
+break;
+case 3:
+Console.WriteLine("Среда");
+break;
+case 4:
+Console.WriteLine("Четверг");
+break;
+case 5:
+Console.WriteLine("Пятница");
+break;
+case 6:
+Console.WriteLine("Суббота");
+break;
+case 7:
+Console.WriteLine("Воскресенье");
+break;
+default:
+Console.WriteLine("Нет такого дня недели");
+break;
+}
+  
+    
